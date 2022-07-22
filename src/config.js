d69ea9e2dc3d739b4cbf82c32317a61f7373a98e
Jeffrey Wong <jvwong@gmail.com>
2022-07-22 14:57:32 -0400

    Add utilities for fetch, database

---
+++ src/config.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+
+const env = (key, defaultVal) => {
+  if (process.env[key] != null) {
+    let val = process.env[key];
+
+    if (_.isInteger(defaultVal)) {
+      val = parseInt(val);
+    } else if (_.isBoolean(defaultVal)) {
+      val = JSON.parse(val);
+    }
+
+    return val;
+  } else {
+    return defaultVal;
+  }
+};
+
+// General
+export const NODE_ENV = env('NODE_ENV', undefined);
+export const LOG_LEVEL = env('LOG_LEVEL', 'info');
+
+// Database
+export const DB_NAME = env('DB_NAME', 'factoid');
+export const DB_HOST = env('DB_HOST', 'localhost');
+export const DB_PORT = env('DB_PORT', 28015);
+export const DB_USER = env('DB_USER', undefined); // username if db uses auth
+export const DB_PASS = env('DB_PASS', undefined); // password if db uses auth
+export const DB_CERT = env('DB_CERT', undefined); // path to a certificate (cert) file if db uses ssl
