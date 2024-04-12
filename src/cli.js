e05562a8d03d8945e51e7f17b2ad8af82503425c
Jeffrey Wong <jvwong@gmail.com>
2024-04-12 15:14:08 -0400

    modularize utilities.

--- src/cli.js
+++ src/cli.js
@@ -1,37 +1,13 @@
 #! /usr/bin/env node
 
-import _ from 'lodash';
-import { writeFile } from 'fs/promises';
 import { program } from 'commander';
-import { load as loadImpl } from './load.js';
-import { json2csv } from './util/format.js';
-import { MAX_DATE, MIN_DATE } from './util/db.js';
-// import getStdin from 'get-stdin';
 
-const formatJSON = obj => JSON.stringify(obj, null, 2);
-const printFormattedJSON = obj => console.log(formatJSON(obj));
-// const writeFormattedJSON = async (obj, file) => await writeFile(file, formatJSON(obj));
-const writeCsv = async (data, file) => await writeFile(file, data);
+import { load as loadImpl } from './load.js';
+import { MAX_DATE, MIN_DATE } from './util/db.js';
+import { toDate, myParseInt } from './util/string.js';
+import { sendOutput } from './util/format.js';
 
 const MAX_NUM_ITEMS = 100000;
-
-function myParseInt (value) {
-  // parseInt takes a string and a radix
-  const parsedValue = parseInt(value, 10);
-  if (isNaN(parsedValue)) {
-    throw new program.InvalidArgumentError('Not a number.');
-  }
-  return parsedValue;
-}
-
-function toDate (str) {
-  const d = new Date(str);
-  if (isNaN(d)) {
-    throw new Error('Invalid Date');
-  } else {
-    return d;
-  }
-}
 
 async function load (options) {
   const lastUpdatedStart = toDate(options.lastUpdatedStart);
@@ -40,22 +16,6 @@ async function load (options) {
   const endDate = toDate(options.end);
   const data = await loadImpl(lastUpdatedStart, lastUpdatedEnd, startDate, endDate, options);
   await sendOutput(data, options);
-}
-
-const formatInfo = info => {
-  const cleaned = _.omit(info, ['authorName']);
-  const { authorName } = info;
-  if (authorName) cleaned.authorName = authorName.replace(/ .*/, '');
-  return cleaned;
-};
-
-async function sendOutput (data, options) {
-  if (options.output) {
-    const clean = data.map(formatInfo);
-    await writeCsv(json2csv(clean), options.output);
-  } else {
-    printFormattedJSON(data);
-  }
 }
 
 async function main () {
